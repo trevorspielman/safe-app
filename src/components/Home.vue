36e03767d080884064489bc213565c0407336a72
Trevor <trevor.spielman@gmail.com>
2018-05-14 16:17:49 -0600

    Small adjustments to the homepage. Added Unlock Safe form.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
-    <h1>Welcome to the SafeApp</h1>
     <div v-if="store.currentSafe.isConnected == false">
+      <h1>Welcome to the SafeApp</h1>
       <h2>Your safe code is: {{this.safeNumber}}</h2>
       <p>Please use your phone to connect to the safe for your deposit/withdrawl</p>
     </div>
     <div v-else>
       <h1>Welcome: {{store.currentSafe.username}}</h1>
       <h3>Current Balance: ${{store.currentSafe.totalAmount.toFixed(2)}}</h3>
+      <form action="submit" @submit.prevent="unlockSafe">
+        <input type="number" placeholder="Transaction ID" v-model="transactionId">
+        <button type="submit" class="btn btn-success">Unlock Safe</button>
+      </form>
     </div>
   </div>
 </template>
@@ -22,7 +26,8 @@
     data() {
       return {
         store,
-        safeNumber: 0
+        safeNumber: 0,
+        transactionId: 0
       }
     },
     methods: {
@@ -30,6 +35,9 @@
       generateSafeNum() {
         this.safeNumber = Math.floor(Math.random() * 9999) + 1000
         store.addSafe(this.safeNumber)
+      },
+      unlockSafe() {
+
       }
     },
     computed: {

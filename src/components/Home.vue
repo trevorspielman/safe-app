fe591c479f1fd7b803c03491fe05756a7c4c4f23
Trevor <trevor.spielman@gmail.com>
2018-06-01 14:04:06 -0600

    Styling added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="home">
-    <div v-if="store.currentSafe.isConnected == true">
+  <div class="home container-fluid">
+    <section class="safeCode" v-if="store.currentSafe.isConnected == true">
       <transaction></transaction>
-    </div>
-    <div v-else>
-      <h1>Welcome to the SafeApp</h1>
+    </section>
+    <div class="safeCode" v-else>
       <h2>Your safe code is: {{this.safeNumber}}</h2>
       <p>Please use your phone to connect to the safe for your deposit/withdrawl</p>
     </div>
@@ -23,13 +22,13 @@
       return {
         store,
         safeNumber: 0,
-        transactionId: 0
+        transactionId: 0,
       }
     },
     methods: {
       //Random Number Generator for safe number. Generates on load of page.
       generateSafeNum() {
-        this.safeNumber = Math.floor(Math.random() * (9999-1000)) + 1000
+        this.safeNumber = Math.floor(Math.random() * (9999 - 1000)) + 1000
         store.currentSafeNumber = this.safeNumber
         store.addSafe(this.safeNumber)
       },
@@ -50,4 +49,11 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .safeCode {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
--- src/components/Transaction.vue
+++ src/components/Transaction.vue
@@ -1,21 +1,70 @@
 <template>
-  <div class="transaction">
-    <h1>Welcome: {{store.currentSafe.username}}</h1>
-    <h3>Current Balance: ${{store.currentSafe.totalAmount}}</h3>
-    <h3>Safe is: <span v-if="store.safeOpen == true">Open</span><span v-else>Closed</span></h3>
-    <div v-if="store.safeOpen == false">
-      <form action="submit" @submit.prevent="unlockSafe">
-        <input type="number" placeholder="Transaction ID" v-model.number.lazy="transactionId">
-        <button type="submit" class="btn btn-success">Unlock Safe</button>
-      </form>
+  <div class="transaction container-fluid">
+    <div class="row">
+      <div class="col-sm-12">
+        <h1>Safe Authorized For: {{store.currentSafe.username}}</h1>
+        <h3>Safe is:
+          <span v-if="store.safeOpen == true">Open</span>
+          <span v-else>Closed</span>
+        </h3>
+      </div>
     </div>
-    <div v-else>
-      <button class="btn btn-danger" @click="lockSafe">Close & Lock Safe</button>
-    </div>
-    <div v-for="transaction in store.safeTransactions">
-      <p>Transaction Type: {{transaction.transType}}</p>
-      <p>Date: {{transaction.createdAt}}</p>
-      <p>Total: ${{transaction.total}}</p>
+    <div class="row">
+      <div class="col-sm-12 col-lg-6 numPad" v-if="store.safeOpen == false">
+        <!-- <form action="submit" @submit.prevent="unlockSafe">
+          <input type="number" placeholder="Transaction ID" v-model.number.lazy="transactionId">
+          <button type="submit" class="btn btn-success">Unlock Safe</button>
+        </form> -->
+  
+        <table class="table">
+          <tbody>
+            <tr>
+              <button class="btn btn-secondary" @click="enterCode(1)">1</button>
+              <button class="btn btn-secondary" @click="enterCode(2)">2</button>
+              <button class="btn btn-secondary" @click="enterCode(3)">3</button>
+            </tr>
+            <tr>
+              <button class="btn btn-secondary" @click="enterCode(4)">4</button>
+              <button class="btn btn-secondary" @click="enterCode(5)">5</button>
+              <button class="btn btn-secondary" @click="enterCode(6)">6</button>
+            </tr>
+            <tr>
+              <button class="btn btn-secondary" @click="enterCode(7)">7</button>
+              <button class="btn btn-secondary" @click="enterCode(8)">8</button>
+              <button class="btn btn-secondary" @click="enterCode(9)">9</button>
+            </tr>
+            <tr>
+              <button class="btn btn-secondary" @click="enterCode(0)">0</button>
+            </tr>
+          </tbody>
+        </table>
+        <h4>Safe Code: {{this.transactionId}}</h4>
+        <button class="btn btn-block btn-success" @click="unlockSafe">Unlock Safe</button>
+        <button class="btn btn-danger mt-4" @click="clear">Clear</button>
+      </div>
+      <div class="col-sm-12 col-lg-6 numPad" v-else>
+        <button class="btn btn-danger" @click="lockSafe">Close & Lock Safe</button>
+      </div>
+      <div class="col-sm-12 col-lg-6 transactions">
+        <h2>Transactions:</h2>
+        <h3>Current Balance: ${{store.currentSafe.totalAmount}}</h3>
+        <table class="table">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">Type</th>
+              <th scope="col">Date</th>
+              <th scope="col">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="transaction in store.safeTransactions">
+              <td>{{transaction.transType}}</td>
+              <td>{{transaction.createdAt}}</td>
+              <td>${{transaction.total}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -29,12 +78,18 @@
     data() {
       return {
         store,
-        transactionId: 0,
+        transactionId: '',
       }
     },
     methods: {
       getTransactions() {
         store.getTransactions()
+      },
+      enterCode(num) {
+        this.transactionId = this.transactionId.concat(num)
+      },
+      clear() {
+        this.transactionId = ''
       },
       unlockSafe() {
         store.unlockSafe(this.transactionId)
@@ -42,7 +97,7 @@
       lockSafe() {
         store.safeOpen = false
         store.lockSafe(this.transactionId)
-        this.transactionId = 0
+        this.transactionId = ''
       }
     },
     computed: {
@@ -58,4 +113,33 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .numPad {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 4rem 0rem;
+    width: 50vw
+  }
+  .numPad .table{
+    width: 50%;
+  }
+
+  .numPad tr {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .table .btn {
+    width: 80px;
+    margin: 5px 0px
+  }
+
+  .transactions {
+    margin: 4rem 0rem;
+    width: 50vw
+  }
+  .transactions td{
+    border-bottom: solid 1px black
+  }
 </style>
